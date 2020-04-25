31098a7af5af1a137d3bec756c0cecc9ff88620a
chiang <chiang@localhost.localdomain>
2020-04-25 17:18:08 +0800

    update

--- dhsjjg/ch5/string.c
+++ dhsjjg/ch5/string.c
@@ -10,10 +10,25 @@ typedef int Status;
 Status StringReverse(char *s);
 Status LongestPalindrome(char * s);
 Status expandSearch(char *s,int left,int right,int *start,int* maxlen,int length);
+
+Status longestOfLengthSubstring(char *s);
+Status isExistCharacter(int index,char *start,char *temp);
+
+Status longestCommonSubsequence(char * text1, char * text2);
+Status reverseLeftWords(char* s, int n);
+
 int main(int argc, char **argv){
     char StringTest1[]="Jiangs";
-    char StringTest2[]="aba";
-    
+    char StringTest2[]="abcabcd";
+    char StringTest3[]="abcedf";
+    char StringTest4[]="adf";
+
+    reverseLeftWords(StringTest1, 2);
+    return OK;
+    longestCommonSubsequence(StringTest3,StringTest4);
+    return OK;
+    longestOfLengthSubstring(StringTest2);
+    return OK;
     LongestPalindrome(StringTest2);
     return OK;
     
@@ -80,4 +95,89 @@ Status expandSearch(char *s,int left,int right,int *start,int* maxlen,int length
         //printf("left =%d right =%d\nmaxlength is %d",left,right,*maxlen);
     }
     return OK;
+}
+
+Status longestOfLengthSubstring(char *s){
+    int i=0,j=0,maxLength=0;
+    char *start;
+    char *tstart;
+    if(strlen(s) < 2 ){
+        printf("The length of string is short\n");
+        return Error;
+    }
+    start=s;
+    for(i=0;i<strlen(s);i++){
+        tstart=s+i;
+        for(j=i+1;j<strlen(s);j++){
+                if(isExistCharacter(j-i,tstart,s+j)){
+                    
+                    if(i==0 && maxLength < j-i){
+                        maxLength=j-i;
+                        start=s+i;
+                        i=j-1;
+                        
+                        break;}
+                }
+                else if(maxLength < j-i +1){
+                    maxLength=j-i+1;
+                    start=s+i;
+                    i=j-1;
+                    break;
+                }
+
+        }
+    }
+    if(maxLength == 0)
+        return Error;
+    printf("The maxLength is %d\nThe LongestSubString is : ",maxLength);
+    for(i=0;i<maxLength;i++){
+        putchar(start[i]);
+    }
+    putchar('\n');
+    return OK;
+}
+
+Status isExistCharacter(int index,char *start,char *temp){
+    int i=0;
+    for(;i<index;i++){
+        if(start[i]==*temp){
+            return Error;
+        }
+    }
+    return OK;
+}
+Status longestCommonSubsequence(char * text1, char * text2){
+    int i;
+    int maxLength=0;
+    char *s1 = text1;
+    char *s2 =text2;
+
+    while(*s1 && *s2){
+        if(*s2 == *s1){
+            s2++;
+            maxLength++;
+        }
+        s1++;
+    }
+    if(maxLength == 0){
+        printf("failed\n");
+        return Error;
+    }
+    printf("maxLength is %d\n longest common sub string is : ",maxLength);
+    for(i=0;i<maxLength;i++)
+        putchar(text2[i]);
+        
+        putchar('\n');
+    return OK;
+}
+Status reverseLeftWords(char* s, int n){
+    char *temp;
+    char *pStr=s;
+    int i=0;
+    temp = malloc(n+1);
+    strncpy(temp,s,n);
+    for(;i<strlen(s)-n;i++)
+        *pStr++=*(pStr+n);
+    strncpy(pStr+1,temp,n);
+    printf("%s",s);
 }
